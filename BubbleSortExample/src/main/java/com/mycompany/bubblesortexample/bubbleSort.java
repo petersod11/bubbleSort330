6be423122e3e2aa1a88a4eefe1d1aa7ea92605db
petersod11 <80416471+petersod11@users.noreply.github.com>
2021-07-06 20:04:13 -0600

    Add files via upload
    add comments

--- BubbleSortExample/src/main/java/com/mycompany/bubblesortexample/bubbleSort.java
+++ BubbleSortExample/src/main/java/com/mycompany/bubblesortexample/bubbleSort.java
@@ -9,11 +9,11 @@ public class bubbleSort {
     
     public static void main(String [] args){
         
-        int i, j, swap;
+        int i, j, swap;//variables to hold values as they are sorted
         
-        int [] arr = {5,11,1,23,56,7,48,7,9,15,9,12 ,0 ,0, 34,6,1,100};
+        int [] arr = {5,11,1,23,56,7,48,7,9,15,9,12 ,0 ,0, 34,6,1,100};//data to be sorted
         
-        for (i=0; i < arr.length; i++){
+        for (i=0; i < arr.length; i++){//nested for loops that check 2 numbers and move them depending on their size
             for (j=0; j < arr.length - 1; j++){
                 if(arr[j]> arr[j+1]){
                     swap = arr[j];
@@ -23,7 +23,7 @@ public class bubbleSort {
             }
         }
         
-        for(int x =0; x<arr.length; x++){
+        for(int x =0; x<arr.length; x++){//displays the final sorted array.
             System.out.print(arr[x]+ ", ");
         }
     }
